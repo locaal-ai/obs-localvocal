66ded7a53239eafef81d2fccf9c15d12f778432d
Ruwen Hahn <haruwenz@twitch.tv>
2025-01-13 17:08:37 +0100

    Add rust CI integration

--- cmake/BuildWebVTT.cmake
+++ cmake/BuildWebVTT.cmake
@@ -1,5 +1,13 @@
 include(FetchContent)
 
+set(Rust_RUSTUP_INSTALL_MISSING_TARGET true)
+
+if(OS_MACOS)
+  if("$ENV{MACOS_ARCH}" STREQUAL "x86_64")
+    set(Rust_CARGO_TARGET "x86_64-apple-darwin")
+  endif()
+endif()
+
 FetchContent_Declare(
   Corrosion
   GIT_REPOSITORY https://github.com/corrosion-rs/corrosion.git
